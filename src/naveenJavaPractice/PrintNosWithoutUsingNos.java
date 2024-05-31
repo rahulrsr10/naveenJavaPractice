f55ff73b0bd82c55f9936b18e38aa25e523a990f
rahulrsr10 <rahulrsr10@outlook.com>
2024-05-31 19:30:56 +0530

    print nos without using nos

---
+++ src/naveenJavaPractice/PrintNosWithoutUsingNos.java
@@ -0,0 +1,18 @@
+package naveenJavaPractice;
+
+public class PrintNosWithoutUsingNos {
+
+	public static void main(String[] args) {
+		int one='A'/'A';
+		String s="..........";
+		for(int i=one;i<=s.length()*s.length();i++) {
+			System.out.println(i);
+		}
+		int d='d'; 
+		for(int j=one;j<=d;j++) {
+			System.out.println(j);
+		}
+
+	}
+
+}
